e36b2574c5afd5e7acdc30e885a047f56bbb7fff
vinzlester <torrevincelester@gmail.com>
2026-02-18 20:18:57 +0800

    Create LAB 1-8.py

---
+++ LAB 1/LAB 1-8.py
@@ -0,0 +1,14 @@
+# Compute the number of seconds for a given number of hours
+
+hours = 2
+SECONDS_PER_HOUR = 3600  # constant value
+
+# Display input
+print("Hours:", hours)
+
+# Calculate and display total seconds
+total_seconds = hours * SECONDS_PER_HOUR
+print("Seconds in hours:", total_seconds)
+
+# End message
+print("Goodbye")
