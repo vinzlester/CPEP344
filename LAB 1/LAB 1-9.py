a3f173e26f322e25c6ea0bb12cf1d4472ff0226f
vinzlester <torrevincelester@gmail.com>
2026-02-18 20:22:58 +0800

    Create LAB 1-9.py

---
+++ LAB 1/LAB 1-9.py
@@ -0,0 +1,19 @@
+# input a float value for variable a here
+a = float(input("Enter first number: "))
+
+# input a float value for variable b here
+b = float(input("Enter second number: "))
+
+# output the result of addition here
+print("Addition:", a + b)
+
+# output the result of subtraction here
+print("Subtraction:", a - b)
+
+# output the result of multiplication here
+print("Multiplication:", a * b)
+
+# output the result of division here
+print("Division:", a / b)
+
+print("\nThat's all, folks!")
